8567353cd68795d7406c9ccecba098c854aeb04a
Crowdbotics <team@crowdbotics.com>
2020-09-18 20:58:13 +0000

    Initial commit.

---
+++ ios/try_to_win_20414Tests/try_to_win_20414Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface try_to_win_20414 : XCTestCase
+
+@end
+
+@implementation try_to_win_20414
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
