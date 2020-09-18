4a292c357dd100aa15df8fb87e0c9bf8950f6d6c
Crowdbotics <team@crowdbotics.com>
2020-09-18 21:04:10 +0000

    Update models

--- backend/try_to_win_20414/wsgi.py
+++ backend/try_to_win_20414/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'try_to_win_20414.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "try_to_win_20414.settings")
 
 application = get_wsgi_application()
